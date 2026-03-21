63e3479c2110920908e32d219508a4a50087689b
Marco Plaitano <marcoplaitano23@gmail.com>
2026-03-21 05:11:29 +0100

    Fix auto light/dark theme switch

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,36 +1,31 @@
-function setTheme(theme, save=false, noauto=true) {
+function setTheme(theme, save=false) {
   if (theme === "dark") {
     document.documentElement.classList.add("dark")
     document.documentElement.classList.remove("light")
-    if (noauto) document.documentElement.classList.remove("auto")
     if (save) localStorage.setItem("theme", theme)
   } else if (theme === "light") {
     document.documentElement.classList.add("light")
     document.documentElement.classList.remove("dark")
-    if (noauto) document.documentElement.classList.remove("auto")
     if (save) localStorage.setItem("theme", theme)
   } else if (theme === "auto") {
     document.documentElement.classList.add("auto")
     document.documentElement.classList.remove("light")
     document.documentElement.classList.remove("dark")
     localStorage.removeItem("theme")
-    setTheme(q.matches ? "dark" : "light", false, false)
+    setTheme(q.matches ? "dark" : "light", false)
   } else return
   try {
     document.querySelector('meta[name="theme-color"]').setAttribute("content", theme === "dark" ? "#18191A" : "#F8F9FA")
-    if (save) setText(theme)
   }
   catch {}
 }
-function setText(theme){document.getElementById("btn-theme").innerText=theme}
 
 const q = window.matchMedia("(prefers-color-scheme: dark)")
 const initialTheme = localStorage.getItem("theme") || "auto"
-setTheme(initialTheme, true, false)
+setTheme(initialTheme, true)
 
 window.onload = function(){
-  setText(initialTheme)
   q.addEventListener("change", function(){
-    if (!("theme" in localStorage)) setTheme(q.matches ? "dark" : "light", false, false)
+    if (!("theme" in localStorage)) setTheme(q.matches ? "dark" : "light", false)
   });
 };
