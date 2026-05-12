0dba2df766f8c81c89d21072622d6476f2769b14
Marco Plaitano <marcoplaitano23@gmail.com>
2026-05-12 11:29:32 +0200

    Remove debug code

--- scripts/streak.js
+++ scripts/streak.js
@@ -41,26 +41,3 @@ function decreaseStreakFreezes() {
     if (currNumStreakFreezes > 0)
         localStorage.setItem("streakFreezes", currNumStreakFreezes - 1);
 }
-
-
-
-const formatDate = (date) => date.toISOString().split('T')[0];
-
-const today = new Date();
-
-const yesterday = new Date(today);
-yesterday.setDate(today.getDate() - 1);
-
-const dayBeforeYesterday = new Date(today);
-dayBeforeYesterday.setDate(today.getDate() - 2);
-
-const TODAY_DATE = new Date().toISOString().split('T')[0]; // "YYYY-MM-DD"
-const YESTERDAY_DATE = formatDate(yesterday);
-const DAY_BEFORE_YESTERDAY_DATE = formatDate(dayBeforeYesterday);
-
-console.log(TODAY_DATE);
-console.log(YESTERDAY_DATE);
-console.log(DAY_BEFORE_YESTERDAY_DATE);
-
-
-increaseStreakFreezes();
