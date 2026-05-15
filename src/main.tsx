366cfcd8167ffa9dd6993ab20594d7a45d33e707
Marco Plaitano <marcoplaitano23@gmail.com>
2026-05-15 14:06:52 +0200

    Add footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import '../style/style.css'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+  </>
 )
